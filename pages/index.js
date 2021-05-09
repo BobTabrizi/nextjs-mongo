435693fd359f660537d03149525b4b59b230a389
BobTabrizi <btabrizi@ucsd.edu>
2021-05-09 14:46:37 -0700

    Testing tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,233 +1,75 @@
 import Head from 'next/head'
 import { connectToDatabase } from '../util/mongodb'
 
-export default function Home({ isConnected }) {
+export default function Home({ properties}) {
+  console.log(properties);
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+          <Head>
+          <title>Mongo DB Testing</title>
+          <link href = "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel = "stylesheet"></link>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
+          </Head>
 
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
+      <div class = "container mx-auto">
+<div class = "row w-full text-center my-4">
+  <h1 class = "text-4x1 font-bold mb-5">TestingMongoDB</h1>
+  
+  </div>
+      <div class = "flex flex-row flex-wrap">
+      {properties && properties.map(property => (
+      <div class = "flex-auto w-1/4 rounded overflow-hidden shadow-lg m-2">
+      <img class = "w-full" src = {property.image}></img>
+      <div class = "px-6 py-4">
+        <div class = "font-bold text-x1 mb-2">{property.name} (Up to {property.guests} guests)</div>
+          <p>{property.address.street}</p>
+          <p class = "text-gray-700 text-base">
+            {property.summary}
+          </p>
+          </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+          <div class = "text-center py-2 my-2 font-bold">
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            <span class = "text-green-500">${property.price}</span>/night
+       </div>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+       <div class = "text-center py-2 my-2">
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+         <button class = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-5 rounded">book</button>
+       </div>
+       </div>
+      ))}
+      </div>
+      </div>
+      </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .subtitle {
-          font-size: 2rem;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+ 
   )
-}
+  }
 
 export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase()
+  const { db } = await connectToDatabase()
+  const data = await db.collection("listingsAndReviews").find().limit(20).toArray();
 
-  const isConnected = await client.isConnected()
+  const properties = JSON.parse(JSON.stringify(data));
+
+const filtered = properties.map(property => {
+
+
+  const price = JSON.parse(JSON.stringify(property.price));
+  return{
+      _id:property._id,
+      name:property.name,
+      image:property.images.picture_url,
+      address: property.address,
+      summary:property.summary,
+      guests:property.accommodates,
+      price:price.$numberDecimal,
+  }
+
+})
 
   return {
-    props: { isConnected },
+    props: { properties:filtered},
   }
 }
